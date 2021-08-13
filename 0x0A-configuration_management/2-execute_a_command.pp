df8596027655af4ea9af51dd92d0cb6bd8278505
Brian Zapata <dzapata397@gmail.com>
2021-08-13 16:44:28 -0500

    changes

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
 # This scrip process kill.
-exec {'kill':
-  command  => '/usr/bin/pkill killmenow',
-  provider => 'shell',
-  returns  => [0, 1],
+exec {'process kill':
+  command => 'pkill killmenow',
+  path    => '/usr/bin/',
 }
