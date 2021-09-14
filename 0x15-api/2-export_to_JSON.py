e7d0fe7350100c5c4cf01f3e4ccd6b6bbad835c1
Brian Zapata <dzapata397@gmail.com>
2021-09-13 23:14:36 -0500

    The script create a file in JSON format

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+""" This script uses the REST API(https://jsonplaceholder.typicode.com)
+and create a file in Json format.
+
+"""
+import requests
+import json
+from sys import argv
+
+if __name__ == "__main__":
+    url_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos').json()
+    url_users = requests.get(
+        'https://jsonplaceholder.typicode.com/users/2').json()
+    user_name = ""
+    my_dict_2 = {}
+    my_list = []
+    for key, value in url_users.items():
+        if key == "username":
+            user_name = value
+    with open("{}.json".format(2), 'w') as file:
+        for _dict in url_todos:
+            if _dict.get("userId") == int(2):
+                my_dict_1 = {}
+                for key, value in _dict.items():
+                    if key == 'title':
+                        my_dict_1["task"] = value
+                    elif key == 'completed':
+                        my_dict_1[key] = value
+                my_dict_1["username"] = user_name
+                my_list.append(my_dict_1)
+        my_dict_2[argv[1]] = my_list
+        file.write(json.dumps(my_dict_2))
