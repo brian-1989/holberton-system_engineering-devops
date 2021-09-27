13dfc5bfcc1ea924d3f0138d9613c4d9df7ecfd2
Brian Zapata <dzapata397@gmail.com>
2021-09-27 16:15:47 -0500

    The top_ten function was created

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" API.
+
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ This function query in the API of reddit.com and
+    return the number of subscribers.
+
+    """
+    response = requests.get(
+        'https://www.reddit.com/r/{}/top.json?limit=10&t=year'.format(
+            subreddit), allow_redirects=False)
+    if response.status_code == 200:
+        data_json = response.json()
+        for key_title in data_json['data']['children']:
+            if 'data' in key_title:
+                print(key_title['data']['title'])
+    else:
+        print(None)
