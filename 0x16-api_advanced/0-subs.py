971ec3c68093edc7a6b7e59d11ad74834d0f0040
Brian Zapata <dzapata397@gmail.com>
2021-09-27 14:19:20 -0500

    The number_of_subscribers function was created

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" API.
+
+"""
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ This function query in the API of reddit.com and
+    return the number of subscribers.
+
+    """
+    response = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit))
+    if response.status_code == 200:
+        data_json = response.json()
+        return data_json['data']['subscribers']
+    return(0)
