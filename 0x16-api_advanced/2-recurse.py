4b72eaec64046789a6d6cac68f355191649d0928
Brian Zapata <dzapata397@gmail.com>
2021-09-27 20:14:45 -0500

    The order of code was organized

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -14,15 +14,16 @@ def recurse(subreddit, hot_list=[], after=""):
     url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
         subreddit, after)
     response = requests.get(
-        url, allow_redirects=False, headers={'User-agent':  'brian-95'})
+        url, allow_redirects=False, headers={'User-Agent':  'brian-95'})
     if response.status_code == 200:
         data_json = response.json()
-        if data_json['data']['after'] is None:
-            return hot_list
-        else:
-            for key_title in data_json['data']['children']:
+        for key_title in data_json['data']['children']:
                 if 'data' in key_title:
                     hot_list.append(key_title['data']['title'])
-            return recurse(subreddit, hot_list, data_json['data']['after'])
+        after = data_json['data']['after']
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
     else:
         return None
